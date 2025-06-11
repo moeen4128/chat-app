03a53663039872b951dd78cb7ba7188e6e021f26
Moeen <moeen.abdulmajeed786@gmail.com>
2025-06-11 12:19:33 +0500

    Test CI workflow

--- lib/view/auth/login_page.dart
+++ lib/view/auth/login_page.dart
@@ -284,7 +284,7 @@ class LoginPage extends StatelessWidget {
                                       },
                                       onCodeAutoRetrievalTimeout:
                                           (verificationId) {
-                                        // Handle timeout if needed
+                                        // TODO: Handle timeout if necessary
                                       },
                                     );
                                   }
